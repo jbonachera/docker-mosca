14f6b18b395b77c15accb00430aef80d64804fb8
Julien BONACHERA <julien.bonachera@smile.fr>
2017-02-04 00:04:41 +0100

    fix kafka

--- server.js
+++ server.js
@@ -8,36 +8,34 @@ var logger = new (winston.Logger)({
 
 logger.level = 'info'
 
-var ascoltatore = {
-          type: 'kafka',
-          kafka: require("kafka-node"),
-          connectionString: "172.20.0.42:2181",
-          clientId: "ascoltatori",
-          groupId: "ascoltatori",
-          defaultEncoding: "utf8",
-          encodings: {
-            image: "buffer"
-          }
-    }
+var backend = {
+    type: "kafka",
+    redis: require('kafka-node'),
+    json: false,
+    connectionString: process.env.ZK_STRING,
+    clientId: "mosca",
+    groupId: "mosca",
+    defaultEncoding: "utf8",
+};
+
 var settings = {
     id: 'mosca',
     port: process.env.NODE_PORT || 1883,
-    logger: { name: 'MoscaServer', level: 'debug' },
+    logger: { name: 'mosca', level: 'debug' },
+    defaultEncoding: "utf8",
     stats: false,
     publishNewClient: false,
     publishClientDisconnect: false,
     publishSubscriptions: false,
-
 }
 
 if (process.env.ZK_STRING != undefined) {
     logger.log('info', 'will use zookeeper at '+process.env.ZK_STRING)
-    settings['backend'] = ascoltatore 
+    settings['backend'] = backend
 };
 
 
 function server(settings) {
-
     var server = new mosca.Server(settings);
     return server
 }
@@ -45,9 +43,30 @@ function server(settings) {
 var app = new server(settings);
 
 app.on("error", function (err) {
-    logger.log('info', err);
+    console.log(err);
 });
 
-app.on("ready", function () {
-    logger.log('info', 'mosca started!');
+
+app.on('clientConnected', function (client) {
+    console.log('Client Connected \t:= ', client.id);
+});
+
+app.on('published', function (packet, client) {
+    console.log("Published :=", packet);
+});
+
+app.on('subscribed', function (topic, client) {
+    console.log("Subscribed :=", topic);
+});
+
+app.on('unsubscribed', function (topic, client) {
+    console.log('unsubscribed := ', topic);
+});
+
+app.on('clientDisconnecting', function (client) {
+    console.log('clientDisconnecting := ', client.id);
+});
+
+app.on('clientDisconnected', function (client) {
+    console.log('Client Disconnected     := ', client.id);
 });
